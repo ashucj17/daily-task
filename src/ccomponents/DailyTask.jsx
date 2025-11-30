cb50f37a8d468c9070aa16d5252434fce61b3291
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-11-30 11:31:26 +0530

    Passing Data Between Components

--- src/ccomponents/DailyTask.jsx
+++ src/ccomponents/DailyTask.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const DailyTask = () =>{
     const [initial, setInitial]= useState(0)
+    const [input, setInput] = useState("");
+    const taskArr = ['task1', 'task2', 'task3', 'task4']
 /*  brief explanation  
 const countArr = useState(0);
 const firstValue = countArr[0]
@@ -9,8 +11,6 @@ const secondValue = countArr[1]
 //    console.log(firstValue)
 //    console.log(secondValue)
 //    console.log(countArr) */
-
-
 // destructured way
 let counter = 0
 const count = ()=>{
@@ -18,12 +18,22 @@ const count = ()=>{
     setInitial( initial + 1)
         console.log("Button clicked",counter)
     }
+
+    const someChange = (e)=>{
+      setInput(e.target.value)
+    }
     return <>
         <h2>Daily Target</h2>
     <p>Task: {initial} </p>
     <p>Task Counter: {initial} </p>
-
     <button onClick={count}>Add Task</button>
+    <input onChange={someChange} type="text" />
+    <h2>{input}</h2>
+    <ul>    
+    {taskArr.map(task=>
+        <li key={task}>{task}</li>
+    )}
+    </ul>
     </>
 }
 
