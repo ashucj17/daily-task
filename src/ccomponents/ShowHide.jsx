66a56de04eb050b7810d9c005427d7736c9bb8be
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-12-02 18:59:48 +0530

    show and hide paragraph

--- src/ccomponents/ShowHide.jsx
+++ src/ccomponents/ShowHide.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShowHide = () => {
+    const[show, setShow] = useState(true)
+    const handleVisibility = () =>setShow(!show)
   return (
-    <div>ShowHide</div>
+    <>
+        {show && (<p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis porro, praesentium ea numquam maxime voluptatum!</p>)}
+        <button onClick={handleVisibility}>{show ? "Hide" : "Show" }</button>
+    </>
   )
 }
 
